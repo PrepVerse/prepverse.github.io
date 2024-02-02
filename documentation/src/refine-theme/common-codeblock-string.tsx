4cb1a48ccc4211af14ba2917bba4ef0e0448e07a
Akash Singh <akashsingh27101998@gmai.com>
2024-02-03 00:35:10 +0530

    Add PrepVerse files

--- documentation/src/refine-theme/common-codeblock-string.tsx
+++ documentation/src/refine-theme/common-codeblock-string.tsx
@@ -101,7 +101,7 @@ export const CodeBlockString = ({
                 // "bg-refine-react-light-code",
                 // "dark:bg-refine-react-dark-code",
                 "bg-gray-200",
-                "dark:bg-refine-bg",
+                "dark:bg-gray-800",
                 "border",
                 "border-gray-300",
                 "dark:border-0",
