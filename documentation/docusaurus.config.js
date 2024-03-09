ef68e6a7788c496a41c40807cc986e562534cee4
Akash Singh <akashsingh03031@gmail.com>
2024-03-09 10:06:23 +0530

    Show my all followers on my about me page

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -127,7 +127,7 @@ const siteConfig = {
             'docusaurus-plugin-dotenv',
             {
                 safe: false, // If false ignore safe-mode, if true load './.env.example', if a string load that file as the sample
-                systemvars: false, // Set to true if you would rather load all system variables as well (useful for CI purposes)
+                systemvars: true, // Set to true if you would rather load all system variables as well (useful for CI purposes)
                 silent: false, //  If true, all warnings will be suppressed
                 expand: false, // Allows your variables to be "expanded" for reusability within your .env file
                 defaults: false, //  Adds support for dotenv-defaults. If set to true, uses ./.env.defaults
