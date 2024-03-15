30ecd84f113abbadf0111d3674cd158784d45a4e
Akash Singh <akashsingh27101998@gmai.com>
2024-03-15 23:43:06 +0530

    Setup ENV Variables

--- documentation/src/context/CommunityStats/index.tsx
+++ documentation/src/context/CommunityStats/index.tsx
@@ -38,6 +38,7 @@ export const CommunityStatsContext = createContext<
 export const CommunityStatsProvider: FC = ({ children }) => {
     const { siteConfig } = useDocusaurusContext();
     const followersAccessKey = siteConfig.customFields.AccessKey;
+    console.log('AccessKey:', followersAccessKey); 
     const [loading, setLoading] = useState(true);
     const [githubStarCount, setGithubStarCount] = useState(0);
     const [githubFollowersCount, setGithubFollowersCount] = useState(0);
