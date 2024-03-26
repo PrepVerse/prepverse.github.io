48cb0459ad7b6d54fb30a5c4620c47a6b703e0a0
Akash Singh <akashsingh27101998@gmai.com>
2024-03-26 17:59:14 +0530

    Redesign blog page layout

--- documentation/src/components/blog/featured-blog-post-items/index.js
+++ documentation/src/components/blog/featured-blog-post-items/index.js
@@ -8,40 +8,47 @@ export const FeaturedBlogPostItems = ({ items }) => {
     return (
         <div
             className={clsx(
-                "blog-sm:max-w-[592px]",
-                "blog-md:max-w-[656px]",
-                "blog-lg:max-w-[896px]",
-                "blog-max:max-w-[1200px]",
-                "w-full",
-                "mx-auto",
-                "px-6 blog-sm:px-0",
-                "py-6 blog-md:py-12",
-                "not-prose",
+                "w-screen",
+                "bg-refine-react-1 dark:bg-gray-800",
             )}
         >
-            <h2
+            <div
                 className={clsx(
-                    "m-0 p-0",
-                    "px-6",
-                    "mb-6 blog-lg:mb-12",
-                    "text-2xl blog-sm:text-[32px] blog-sm:leading-10",
-                    "dark:text-refine-cyan-alt dark:drop-shadow-[0_0_30px_rgba(71,235,235,0.25)]",
-                    "text-refine-blue drop-shadow-[0_0_30px_rgba(51,51,255,0.3)]",
-                    "font-semibold",
+                    "blog-sm:max-w-[592px]",
+                    "blog-md:max-w-[656px]",
+                    "blog-lg:max-w-[896px]",
+                    "blog-max:max-w-[1200px]",
+                    "w-full",
+                    "mx-auto",
+                    "px-6 blog-sm:px-0",
+                    "py-6 blog-md:py-12",
+                    "not-prose",
                 )}
             >
-                Featured Posts
-            </h2>
+                <h2
+                    className={clsx(
+                        "m-0 p-0",
+                        "px-6",
+                        "mb-6 blog-lg:mb-12",
+                        "text-2xl blog-sm:text-[32px] blog-sm:leading-10",
+                        "dark:text-refine-cyan-alt dark:drop-shadow-[0_0_30px_rgba(71,235,235,0.25)]",
+                        "text-refine-blue drop-shadow-[0_0_30px_rgba(51,51,255,0.3)]",
+                        "font-semibold",
+                    )}
+                >
+                    Featured Posts
+                </h2>
 
-            <div className="grid grid-cols-1 blog-md:grid-cols-2 blog-max:grid-cols-3 gap-6">
-                {items.map(({ content: BlogPostContent }) => (
-                    <BlogPostProvider
-                        key={BlogPostContent.metadata.permalink}
-                        content={BlogPostContent}
-                    >
-                        <FeaturedBlogPostItem />
-                    </BlogPostProvider>
-                ))}
+                <div className="grid grid-cols-1 blog-md:grid-cols-2 blog-max:grid-cols-3 gap-6">
+                    {items.map(({ content: BlogPostContent }) => (
+                        <BlogPostProvider
+                            key={BlogPostContent.metadata.permalink}
+                            content={BlogPostContent}
+                        >
+                            <FeaturedBlogPostItem />
+                        </BlogPostProvider>
+                    ))}
+                </div>
             </div>
         </div>
     );
